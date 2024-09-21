5f71fcfc67c12d1fd225d857c8bdc633734029fc
realtimestert <istewpotter1@gmail.com>
2024-09-20 22:38:48 -0400

    Modified CSS and HTML, trying to get JS to work

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,1 +1,6 @@
 // Testing
+const currentYear = new Date().getFullYear()
+const lastModified = document.lastModified;
+
+document.getElementById("copyright-year").textContent = currentYear;
+document.getElementById("last-modified").textContent = 'Last Modified: ${lastModified}';
