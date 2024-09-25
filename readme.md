67c71e384107a34fcc63c46cd7c363ae4fc35581
realtimestert <istewpotter1@gmail.com>
2024-09-25 10:20:19 -0400

    conformed to class standards

--- readme.md
+++ readme.md
@@ -3,6 +3,4 @@ This is a test.
 
 Keep the pace up for midweek as well. Have things done by Wednesday. 
 
-Git 'r done!
-
 Before Saturday have all weekly work done.
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,6 +1,15 @@
 // Testing
-const currentYear = new Date().getFullYear()
-const lastModified = document.lastModified;
+//const currentYear = new Date().getFullYear()
+//const lastModified = date.lastModified;
 
-document.getElementById("copyright-year").textContent = currentYear;
-document.getElementById("last-modified").textContent = 'Last Modified: ${lastModified}';
+//document.getElementById("copyright-year").textContent = currentYear;
+//document.getElementById("last-modified").textContent = 'Last Modified: ${lastModified}';
+
+const options = {
+    year: "numeric",
+};
+year.innerHTML = new Date().toLocaleDateString("en-US", options);
+
+let modifiedDate = document.lastModified;
+
+lastModified.innerHTML = `Last Modified: ${modifiedDate}`;
