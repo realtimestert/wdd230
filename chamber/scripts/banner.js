a9b7e4026b35b15ec27a78b4d712609801478638
realtimestert <istewpotter1@gmail.com>
2024-10-30 13:58:30 -0400

    added banner

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,1 +1,8 @@
 const day = new Date().getDay();
+if (day !== 1 && day !== 2 && day !== 3) {
+    document.querySelector('.banner').style.display = "none";
+}
+const closeButton = document.querySelector('.close-banner');
+closeButton.addEventListener('click', () => {
+    document.querySelector(".banner").style.display = "none";
+})
